9e7333c6237fd0f4e804e89e27046e36b18a107f
kakegawa <pedestrian.deck.2023@gmail.com>
2024-02-28 06:45:39 +0900

    aaa

--- bankapp/bunk_files/p_css/accountConfirm.js
+++ bankapp/bunk_files/p_css/accountConfirm.js
@@ -1,5 +1,5 @@
 /** アカウント確認 **/
-const transmission = getQueryParams('transmission');
+const transmission = getQueryParams('id');
 
 window.addEventListener('DOMContentLoaded', function () {
     document.getElementsByTagName('main')[0].innerHTML = document.getElementsByTagName('main')[0].innerHTML + '<p>' + transmission + '</p>';
--- bankapp/bunk_files/p_css/accountIssuance.js
+++ bankapp/bunk_files/p_css/accountIssuance.js
@@ -1,8 +1,21 @@
 /** アカウント発行 **/
+// スプレッドシートのウェブアプリURLを定義
+const WEBAPPURL = 'https://script.google.com/macros/s/AKfycbzzp0P-lsjnNPbdJTZL3V58TTUAuWov3cRK_Qc0rJgjZY9tYRvTNthoKaptnI7C_AFypQ/exec';
+
 window.addEventListener('DOMContentLoaded', function () {
     // 要素を取得
     const name = document.getElementById('bai_name'); // ニックネームの入力欄
     const confirmBtn = document.getElementById('bai_confirmBtn'); // 発行するボタン
+
+    // ローダーの生成
+    createLoader('よみこみ<ruby>中<rt>ちゅう</rt></ruby>');
+
+    // 画面読込完了時の処理
+    window.addEventListener('load', function () {
+        // ローダーを非表示
+        hideLoader();
+    });
+
 
     // ニックネームの入力欄に入力時の処理
     name.addEventListener('input', function () {
@@ -19,58 +32,55 @@ window.addEventListener('DOMContentLoaded', function () {
     name.addEventListener('keydown', function (e) {
         if (e.key === 'Enter') {
             if (this.value.trim().length != 0) {
+                this.blur();
                 confirmBtn.click();
             }
         }
     });
 
-    // 引数で指定した桁数のハッシュ値を生成する関数
-    // （生成したハッシュ値をキーとしてスプレッドシートに保存し、次の画面で取引を照合するため）
-    function getHash(length) {
-        const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        let str = '';
-        for (let i = 0; i < length; i++) {
-            str += chars[Math.floor(Math.random() * chars.length)];
-        }
-        return str;
-    }
-
     // 発行ボタン押下時の処理
-    confirmBtn.addEventListener('click', function (e) {
-        // ローダーを表示
-        showLoader();
+    confirmBtn.addEventListener('click', function () {
+        if (!this.classList.contains('bai_confirmBtn_disabled')) {
+            // ローダーを表示
+            showLoader();
 
-        // スプレッドシートのウェブアプリURLを定義
-        const WEBAPPURL = 'https://script.google.com/macros/s/AKfycbzzp0P-lsjnNPbdJTZL3V58TTUAuWov3cRK_Qc0rJgjZY9tYRvTNthoKaptnI7C_AFypQ/exec';
+            // テキストフィールドの値（入力されたニックネーム）を取得
+            const nameValue = name.value;
 
-        // 送信データを定義
-        let sendData = {
-            "name": name.value
-        };
+            // 送信データを定義
+            let sendData = {
+                "name": nameValue
+            };
 
-        // 送信パラメータを定義
-        const postparam = {
-            "method": "POST",
-            "Content-Type": "application/json",
-            "body": JSON.stringify(sendData)
-        };
+            // 送信パラメータを定義
+            const postparam = {
+                "method": "POST",
+                // "mode": "no-cors",
+                "Content-Type": "application/json",
+                "body": JSON.stringify(sendData)
+            };
 
-        // 指定されたURLにリクエストを送信
-        fetch(WEBAPPURL, postparam).then(function (response) {
-            // GASからのレスポンスをJSON型に変換
-            return response.json();
-        }).then(function (data) {
-            if (data.newId !== undefined) {
-                // レスポンスがちゃんと返ってきた場合
-                location.href = setQueryParams('./accountConfirm.html', { "id": data.newId });
-            } else {
-                // レスポンスが返ってこなかった場合
-                console.log('レスポンス返ってこない、、、');
-            }
-        }).catch(function (error) {
-            console.log(error);
-            window.alert(error);
-        });
+            // 指定されたURLにリクエストを送信
+            fetch(WEBAPPURL, postparam).then(function (response) {
+                if (response.ok) {
+                    // レスポンスが返ってきた場合
+                    // レスポンスデータをJSON形式に変換
+                    return response.json();
+                }
+            }).then(function (data) {
+                if (data.newId !== undefined) {
+                    // レスポンスがちゃんと返ってきた場合
+                    location.href = setQueryParams('./accountConfirm.html', { "id": data.newId });
+                } else {
+                    // レスポンスが返ってこなかった場合
+                    hideLoader();
+                    console.log('レスポンス返ってこない、、、');
+                }
+            }).catch(function (error) {
+                hideLoader();
+                console.log(error);
+            });
+        }
     });
 });
 
