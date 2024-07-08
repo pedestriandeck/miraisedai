b0c6e95502c4de9c77fb05ab647dcdfe56b4ce20
yasui <jhondoe@example.com>
2024-07-08 14:17:38 +0900

    0708最新化

--- bankapp/bunk_files/p_css/test.js
+++ bankapp/bunk_files/p_css/test.js
@@ -1,24 +1,39 @@
-const depositAmount = document.getElementsByClassName('test_detail_depositAmount')[0];
-const balanceAmount = document.getElementsByClassName('test_detail_balanceAmount')[0];
-const apiURL = 'https://script.google.com/macros/s/AKfycbw5gk2RTtESqAjUBkeQvo_MRHaUQL4sZaNc0AO2XcQ/dev';
 
-let postparam = {
-    "method": "POST",
-    "mode": "no-cors",
-    "Content-Type": "application/x-www-form-urlencoded"
-    // "body": JSON.stringify(SendDATA)
-}
 
-async function setAmountList() {
-    const response = await fetch(apiURL,postparam);
-    const data = response.json();
-    console.log(data);
 
-    for(let i = 0; i <= data.length; i++){
-        let new_element = document.createElement('p');
-        new_element.textContent = data.amount;
-        depositAmount.appendChild(new_element);
-    }
-}
 
-setAmountList();
+
+
+const db = new Dexie('TestAppDB');
+
+db.version(1).stores({
+    TestDB: '++number, timeStump, userId, amount, balance'
+});
+
+const testTable = db.table('TestDB');
+
+const depositAmount =  document.getElementsByClassName('test_detail_depositAmount')[0];
+const balanceAmount =  document.getElementsByClassName('test_detail_balanceAmount')[0];
+
+
+
+const deployUrl ='https://script.google.com/macros/s/AKfycbzeTxaHCM0GhsxYywR13LzjgVo3nDfFJNwCABu73fAB90IR7UA7cq-KJmtBl4DXLmgV/exec';
+
+
+fetch(deployUrl).then(function(response) {
+    const dataList = response.json();
+    console.log(dataList);
+}).then(function() {
+    // testTable.open();
+    testTable.put(deployUrl,'number');
+    testTable.orderBy('timeStump').each(function(test) {
+        let newElement = document.createElement('p');
+        newElement.innerText = test.amount; 
+        depositAmount.appendChild(newElement);
+    });
+    // testTable.close();
+    console.log('end');
+});
+
+
+
